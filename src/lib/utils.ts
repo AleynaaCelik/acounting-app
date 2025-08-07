b33dacbfb37af68f27fa41033b33147b1bd93011
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-08-07 22:45:20 +0300

    Added script

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+// src/lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('tr-TR', {
+    style: 'currency',
+    currency: 'TRY'
+  }).format(amount)
+}
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('tr-TR').format(date)
+}
